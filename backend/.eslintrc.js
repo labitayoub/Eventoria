4252663c4fe966e0f229d20d90adaebc3a976ac8
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2026-02-04 09:33:13 +0100

    EV-7 Configurer ESLint + Prettier

---
+++ backend/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@nestjs'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
